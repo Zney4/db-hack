126a1664ebf176eb5e011a03c13a4d7a03e75256
Zney4 <134953901+Zney4@users.noreply.github.com>
2025-01-13 18:49:25 +0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,69 @@
+# python manage.py runserver
+# python manage.py shell
+
+from datacenter.models import (
+    Schoolkid,
+    Teacher,
+    Mark,
+    Chastisement,
+    Commendation,
+    Lesson,
+    Subject,
+)
+import random
+
+
+def fix_marks(name):
+    schoolkid = Schoolkid.objects.filter(full_name__contains=name).get()
+    point = Mark.objects.filter(schoolkid=schoolkid, points__lte=3).update(points=5)
+    print(point)
+    point = Mark.objects.filter(schoolkid=schoolkid, points__lte=3)
+    print(point.count())
+
+
+fix_marks(name="Фролов Иван Григорьевич")
+
+
+def remove_chastisements(name):
+    schoolkid = Schoolkid.objects.filter(full_name__contains=name).get()
+    point = Mark.objects.filter(schoolkid=schoolkid, points__lte=3).update(points=5)
+    print(point)
+    point = Mark.objects.filter(schoolkid=schoolkid, points__lte=3)
+    print(point.count())
+    comments = Chastisement.objects.filter(schoolkid=schoolkid)
+    comments.delete()
+    print(comments)
+
+
+remove_chastisements(name="Голубев Феофан Владленович")
+
+
+def create_commendation(name):
+    schoolkid = Schoolkid.objects.filter(full_name__contains=name).get()
+    print(schoolkid)
+    subjects = Subject.objects.filter(year_of_study=6, title="Музыка")
+    lessons = Lesson.objects.filter(
+        year_of_study=6, group_letter="А", subject=subjects[0]
+    ).order_by("date")
+    teachers = Teacher.objects.filter(full_name__contains="Селезнева Майя Макаровна")
+    dates = lessons[0].date
+    compliment_list = [
+        "Молодец!",
+        "Отлично!",
+        "Прекрасно!",
+        "Ты сегодня прыгнул выше головы!",
+        "Я вижу, как ты стараешься!",
+        "Мы с тобой не зря поработали!",
+        "Именно этого я давно ждал от тебя!",
+        "Ты меня очень обрадовал!",
+    ]
+    compliment = Commendation.objects.create(
+        subject=subjects[0],
+        schoolkid=schoolkid,
+        teacher=teachers[0],
+        created=dates,
+        text=random.choice(compliment_list),
+    )
+
+
+create_commendation(name="Фролов Иван Григорьевич")
