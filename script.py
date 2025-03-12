e62e70c7a7282b69b3eda3aea243d4030657c9bf
Zney4 <134953901+Zney4@users.noreply.github.com>
2025-03-12 18:39:26 +0300

    add code

--- script.py
+++ script.py
@@ -82,13 +82,13 @@ print(result)
 
 def create_commendation(schoolkid):
     subjects = get_object_or_404(Subject, year_of_study=6, title="Музыка")
-    if Lesson:
-        lessons = Lesson.objects.filter(
-            year_of_study=6, group_letter="А", subject=subjects
-        ).order_by("date")
-        teachers = get_object_or_404(
-            Teacher, full_name__contains="Селезнева Майя Макаровна"
-        )
+    lessons = Lesson.objects.filter(
+        year_of_study=6, group_letter="А", subject=subjects
+    ).order_by("date")
+    teachers = get_object_or_404(
+        Teacher, full_name__contains="Селезнева Майя Макаровна"
+    )
+    if lessons:
         dates = lessons[0].date
         compliment = Commendation.objects.create(
             subject=subjects,
